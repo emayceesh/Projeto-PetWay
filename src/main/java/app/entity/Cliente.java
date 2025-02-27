d63bf5941a824d2f304d23db6183457ba40b87af
Patryk Rennan <165728048+emayceesh@users.noreply.github.com>
2025-02-27 14:42:37 -0300

    Commit entidade cliente

--- src/main/java/app/entity/Cliente.java
+++ src/main/java/app/entity/Cliente.java
@@ -1,5 +1,38 @@
 package app.entity;
 
-public class Cliente {
+import org.hibernate.validator.constraints.br.CPF;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank(message = "O nome não pode estar vazio")
+	private String nomeCliente;
+	
+	@CPF
+	private String cpf;
+	
+	private String telefone;
+	
+	private Boolean cadastroCompleto;
+	
+	
 }
