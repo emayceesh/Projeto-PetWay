1df05f18818eae0da69c8c5936683d2af401a6fe
Patryk Rennan <165728048+emayceesh@users.noreply.github.com>
2025-04-29 21:07:47 -0300

    commit teste 01

---
+++ src/test/java/app/controller/test/ClienteControllerTests.java
@@ -0,0 +1,14 @@
+package app.controller.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import app.controller.ClienteController;
+
+@SpringBootTest
+public class ClienteControllerTests {
+
+	@Autowired
+	ClienteController clienteController;
+	
+}
--- src/test/java/app/service/test/ClienteServiceTests.java
+++ src/test/java/app/service/test/ClienteServiceTests.java
@@ -1,5 +1,8 @@
 package app.service.test;
 
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class ClienteServiceTests {
 
 }
